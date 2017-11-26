3767cec8fccde38aaf71ffe483b3691b1955a6d5
Hank Lewis <hank@modernatomic.com>
2017-11-26 12:26:34 -0800

    Revert "Merge branch 'master' of https://github.com/swagelerator1/complex-ACP-LASA"
    This reverts commit aa5bdfdef2b544c1555bb4ec4d74f03a62b9382b, reversing
    changes made to bd41024d2acf72800454a50ca0d7ed085c97e1ca.

--- arg.cpp
+++ arg.cpp
@@ -1,6 +1,4 @@
 #include "complex.h"
 double arg(const complex c) {
-	double t = atan(c.b/c.a);
-	if(t < 0 && c.a < 0) return M_PI+t;
-	else return t;
+      return atan(c.b/c.a);
     }
--- asin.cpp
+++ asin.cpp
@@ -1,5 +1,5 @@
 #include "complex.h"
 complex asin(const complex c) { //arcsin(z) = -iln(iz + sqrt(1+z^2))
       complex i(0, 1);
-	  return -1*i*log(i*c+sqrt(1-pow(c, 2)));
+	  return -1*i*log(i*c+sqrt(1-pow(c, 2.0)));
     }
